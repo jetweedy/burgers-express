db6407d2eba956d83f755c90ca06cafe3426ae50
Jon Tweedy <jetweedy@gmail.com>
2019-05-06 16:41:32 +0000

    m

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ var connection = mysql.createConnection({
 //	user: "mydb",
 //	password: "mydb",
 //	database: "bamazon"
-  host: (process.env.DB_HOST || "localhost"),
+  host: (process.env.DB_HOST || "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"),
   port: 3306,
   user: (process.env.DB_USERNAME || "username"),
   password: (process.env.DB_PASSWORD || "password"),
-  database: (process.env.DB_DATABASE || "burgers_db")
+  database: (process.env.DB_DATABASE || "w2mbe8v7gqgoz5j9")
 });
 connection.connect(err => {
   if (err) {
