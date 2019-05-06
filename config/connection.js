e5c6c4d5d2a3528f1b624070b71947e16a8771fb
Jon Tweedy <jetweedy@gmail.com>
2019-05-06 16:52:26 +0000

    m

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,6 @@
 require("dotenv").config();
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-//	host: "localhost",
-//	user: "mydb",
-//	password: "mydb",
-//	database: "bamazon"
-  host: (process.env.DB_HOST || "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"),
-  port: 3306,
-  user: (process.env.DB_USERNAME || "username"),
-  password: (process.env.DB_PASSWORD || "password"),
-  database: (process.env.DB_DATABASE || "w2mbe8v7gqgoz5j9")
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 connection.connect(err => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
