ad0e25b0e998a774ce1ae6d1dfa263beea7dbb46
= <=>
2020-12-06 22:18:52 +0000

    M

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
 var mysql = require("mysql");
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(process.env.DB_URL);
+/*
+var connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	database: process.env.DB_DATABASE,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD
+});
+*/
 connection.connect(err => {
   if (err) {
     console.error(`error connecting: ${err.stack}`);
